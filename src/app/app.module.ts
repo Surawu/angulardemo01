384edcbcee74c2fb5cb6db4df7830085f97a7a7b
Surawu <surawu@outlook.com>
2021-03-10 10:17:56 +0800

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HeroBirthday1Component } from './hero-birthday1/hero-birthday1.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroBirthday1Component ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
